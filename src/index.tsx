d3fab8254a8f97c64f59140f95e3bf72f9bf0748
Eduard Jacobs <edoter92@gmail.com>
2021-12-23 03:52:44 -0800

    exports

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+export * from "./@types/global";
+export * from "./Components/Grid";
+export * from "./Components/Input";
+export * from "./Components/Divider";
+export * from "./Components/ContentBox";
+
+export { default as Layout } from "./Layout";
+export { default as Card, CardProps } from "./Components/Card";
+export { default as Field, FieldProps } from "./Components/Field";
+export { default as Badge, BadgeProps } from "./Components/Badge";
+export { default as Radio, RadioProps } from "./Components/Radio";
+export { default as Button, ButtonProps } from "./Components/Button";
+export { default as Section, SectionProps } from "./Components/Section";
+export { default as Table, TableRow, Content } from "./Components/Table";
+export { default as Dropdown, DropdownProps } from "./Components/Dropdown";
+export { default as Checkbox, CheckboxProps } from "./Components/Checkbox";
+export { default as Select, Option, SelectProps } from "./Components/Select";
+export { default as Thumbnail, ThumbnailProps } from "./Components/Thumbnail";
+export { Input, InputEmail, InputNumber, InputProps } from "./Components/Input";
+export {
+  default as Breadcrumb,
+  BreadcrumbProps,
+} from "./Components/Breadcrumb";
